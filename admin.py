cd26b6829a6f4b5c4fb3ee66b7a33b16bc854f0f
Luis E. Chavez <Goldenturtle121@gmail.com>
2025-04-01 18:05:56 -0400

    Fixed the file structure
    cleaned up and sorted out the file structure.

--- admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-# Register your models here.
-
-class UserAdmin(admin.ModelAdmin):
-  list_display = ('username', 'email', 'password') #Display these fields in the admin list
-  search_fields = ('username', 'email') # searchs for a username/email
-
