127a16797795547b31c24b96374a7792e50a49e3
Your Name <Goldenturtle121@gmail.com>
2025-04-08 17:50:51 -0400

    Restructured once again cause terminal is complaining

--- events/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),  # Calls the 'home' view when visiting the main page
-]
--- events/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Handles requests & returns responses (HTML, JSON, etc.)
-from django.http import HttpResponse
-
-def home(request):
-    return HttpResponse("Welcome to Event Management App!")
