043d3928fb1a13d6d647097703b277d3a2c9a2f2
Your Name <Goldenturtle121@gmail.com>
2025-04-30 00:05:28 -0400

    Final final final commit
    added more comments

--- EventManagementApp/users/sms_utils.py
+++ EventManagementApp/users/sms_utils.py
@@ -4,10 +4,10 @@ import os
 # Optionally, load from environment variables
 TWILIO_ACCOUNT_SID = 'your_account_sid'
 TWILIO_AUTH_TOKEN = 'your_auth_token'
-TWILIO_PHONE_NUMBER = '+1XXXXXXXXXX'  # Your Twilio number
+TWILIO_PHONE_NUMBER = '+1XXXXXXXXXX'  # The number registered with a users account gets stored and the twilio library handles the SMS stuff.
 
 client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-
+    # SMS function itself
 def send_sms(to, message):
     client.messages.create(
         body=message,
--- EventManagementApp/users/urls.py
+++ EventManagementApp/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from users.views import registration_view, home_view, dashboard_view, login_view
 from django.contrib.auth import views as auth_views
 from .import views
-
+# took a while for me to realize I mispelled register, 
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.registration_view, name='register'),
